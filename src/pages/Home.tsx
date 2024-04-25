acfdd220fa0717143f4ccf62d02196e519579ec1
Matheus Sanches <methidezeeni@gmail.com>
2024-04-24 21:05:53 -0300

    chore: merge with main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
-import App from "../App";
+import { Box } from "@chakra-ui/react";
+import { ChatContainer } from "../components/chat/chat-container";
+import { ChatBox } from "../components/chat/chatbox";
 
 export function Home() {
-  return <App />
+  return <div>
+    <Box m={3}>
+        <ChatContainer>
+          <ChatBox position="left">teste de mensagem na esquerda</ChatBox>
+          <ChatBox position="left">teste de mensagem na esquerda denovo</ChatBox>
+
+          <ChatBox>teste de mensagem na direita</ChatBox>
+          <ChatBox>msg de loading:</ChatBox>
+          <ChatBox state="loading" position="left" />
+          <ChatBox>msg de erro:</ChatBox>
+          <ChatBox state="error" position="left" />
+        </ChatContainer>
+      </Box>
+  </div>
 }
