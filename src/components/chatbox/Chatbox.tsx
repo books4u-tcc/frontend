8660309caf424688f17b700f59e1f8c50ba2c761
Rodolfo <rodolfo.b.liberado@gmail.com>
2024-04-28 22:54:22 -0300

    Start component structures

---
+++ src/components/chatbox/Chatbox.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Chatbox(){
+    return(
+        <Flex w="80%" h="100rem" >  
+            w
+        </Flex>
+    )
+}
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import { Box, Flex } from "@chakra-ui/react";
+
+export default function Sidebar(){
+    return(
+        <Flex w="20%">
+            w
+        </Flex>
+    )
+}
