445d0b0488ddfe8f87af89b736b6b9e9e5521766
Matheus Dias <methidezeeni@gmail.com>
2024-05-26 19:07:45 -0300

    chore: fix

--- src/components/chat/chat-container.tsx
+++ src/components/chat/chat-container.tsx
@@ -7,6 +7,7 @@ export function ChatContainer() {
   return (
     <Flex w="100%" h="100%" minW="600px" py={3} pr={3} maxH="100dvh">
       <Flex
+        flexDirection="column"
         w="100%"
         h="100%"
         borderRadius="3xl"
@@ -14,8 +15,8 @@ export function ChatContainer() {
         border="1px solid #E2E2E2"
         boxShadow="1px 1px 18px 3px rgb(0 0 0 / 6%)"
       >
-        <Flex flex={1} flexDirection="column">
-          <Flex overflowY="auto">
+        <Flex flex={1} flexDirection="column" height="100%">
+          <Flex overflowY="auto" flex={1}>
             <Container
               maxWidth="container.xl"
               flex={1}
