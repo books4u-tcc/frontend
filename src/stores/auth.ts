bd245179d0973d2ab6880c24b63411ed7598fdda
Matheus Sanches <methidezeeni@gmail.com>
2024-10-07 21:14:35 -0300

    chore: auth token store

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { apiClient } from "../components/client/api";
 
 interface AuthStore {
   authenticated: boolean,
@@ -14,10 +15,24 @@ const { setState, getState } = useAuthStore
 
 export const authStoreActions = {
   login(token: string) {
+    localStorage.setItem('auth-token', token)
     setState({ authenticated: true, token })
   },
 
   logout() {
+    localStorage.removeItem('auth-token')
     setState({ authenticated: false })
+  },
+
+  async boot() {
+    const token = localStorage.getItem('auth-token')
+    setState({ token })
+    try {
+      await apiClient.getProfile()
+      setState({ authenticated: true })
+    } catch (error) {
+      console.error(error)
+      setState({ authenticated: true, token: null })
+    }
   }
 }
